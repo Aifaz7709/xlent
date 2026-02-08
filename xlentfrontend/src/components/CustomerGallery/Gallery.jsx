17e59f7e493bfd300bc12bf36daa2aee34aca1e6
Aifaz7709 <ayanaifazkhan@gmail.com>
2026-02-09 02:40:01 +0530

    UI  changes

--- xlentfrontend/src/components/CustomerGallery/Gallery.jsx
+++ xlentfrontend/src/components/CustomerGallery/Gallery.jsx
@@ -14,10 +14,10 @@
         }}>Smiles & Miles</h2>
               <div className="row vw-90 px-2 d-flex align-items-center g-4 g-md- ">
                 <div className="col-md-2" data-aos="fade-up"><img src="/gallery/1.jpg" className="rounded shadow img-fluid" alt="gallery" /></div>
-                <div className="col-md-2" data-aos="fade-up" data-aos-delay="200"><img src="/gallery/2.webp" className="img-fluid rounded shadow" alt="gallery" /></div>
+                <div className="col-md-2" data-aos="fade-up" data-aos-delay="200"><img src="/gallery/2.jpg" className="img-fluid rounded shadow" alt="gallery" /></div>
                 <div className="col-md-3" data-aos="fade-up" data-aos-delay="400"><img src="/gallery/3.jpg" className="img-fluid rounded shadow" alt="gallery"/></div>
                 <div className="col-md-3" data-aos="fade-up" data-aos-delay="600"><img src="/gallery/4.jpg" className="img-fluid rounded shadow" alt="gallery" /></div>
-                <div className="col-md-2" data-aos="fade-up" data-aos-delay="800"><img src="/gallery/5.jpg" className="rounded shadow img-fluid" alt="gallery" /></div>
+                <div className="col-md-2" data-aos="fade-up" data-aos-delay="800"><img src="/gallery/5.avif" className="rounded shadow img-fluid" alt="gallery" /></div>
               </div>
             </div>
           </div>
